f4fb4f4ed5be4d782321dda57342c77d76580ac0
Ben Jackson <puremourning@gmail.com>
2022-03-20 20:54:10 +0000

    First files

---
+++ conanfile.py
@@ -0,0 +1,40 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake, cmake_layout
+
+
+class YcmdConan(ConanFile):
+    name = "ycmd"
+    version = "2.0.0"
+
+    # Optional metadata
+    license = "Apache 2.0"
+    author = "Ben Jackson puremourning+ycmd@gmail.com"
+    url = "https://github.com/ycm-core/ycmd"
+    description = "A code comprehension server"
+
+    # Binary configuration
+    settings = "os", "compiler", "build_type", "arch"
+
+    # Sources are located in the same place as this recipe, copy them to the
+    # recipe
+    exports_sources = "CMakeLists.txt", "src/*"
+
+    # Requirements
+    requires = ( "Boost/1.78.0" )
+
+    def layout(self):
+        cmake_layout(self)
+        del self.cpp.source.includedirs
+
+    def generate(self):
+        CMakeToolchain(self).generate()
+        CMakeDeps(self).generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
---
+++ src/ycmd.cpp
@@ -0,0 +1,8 @@
+#include <cstdio>
+
+int main()
+{
+  printf( "Hi\n" );
+
+  return 0;
+}
