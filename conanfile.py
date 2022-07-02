96a93d65bb7c1d4bbf04e7a327d404aae3ebd2e0
Ben Jackson <puremourning@gmail.com>
2022-07-02 11:09:41 +0100

    Some python linting

--- conanfile.py
+++ conanfile.py
@@ -3,44 +3,44 @@ from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake, cmake_layout
 
 
 class YcmdConan(ConanFile):
-    name = "ycmd"
-    version = "2.0.0"
+  name = "ycmd"
+  version = "2.0.0"
 
-    # Optional metadata
-    license = "Apache 2.0"
-    author = "Ben Jackson puremourning+ycmd@gmail.com"
-    url = "https://github.com/ycm-core/ycmd"
-    description = "A code comprehension server"
+  # Optional metadata
+  license = "Apache 2.0"
+  author = "Ben Jackson puremourning+ycmd@gmail.com"
+  url = "https://github.com/ycm-core/ycmd"
+  description = "A code comprehension server"
 
-    # Binary configuration
-    settings = "os", "compiler", "build_type", "arch"
+  # Binary configuration
+  settings = "os", "compiler", "build_type", "arch"
 
-    # Sources are located in the same place as this recipe, copy them to the
-    # recipe
-    exports_sources = "CMakeLists.txt", "src/*"
+  # Sources are located in the same place as this recipe, copy them to the
+  # recipe
+  exports_sources = "CMakeLists.txt", "src/*"
 
-    # Requirements
-    requires = (
-      "boost/1.78.0",
-      "nlohmann_json/3.10.5",
-      "abseil/20211102.0",
-    )
+  # Requirements
+  requires = (
+    "boost/1.78.0",
+    "nlohmann_json/3.10.5",
+    "abseil/20211102.0",
+  )
 
-    def layout(self):
-      cmake_layout(self)
+  def imports(self):
+    self.copy('*', src='bin', dst='bin')
 
-    def generate(self):
-      CMakeToolchain(self).generate()
-      CMakeDeps(self).generate()
+  def layout(self):
+    cmake_layout(self)
 
-    def build(self):
-      cmake = CMake(self)
-      cmake.configure()
-      cmake.build()
+  def generate(self):
+    CMakeToolchain(self).generate()
+    CMakeDeps(self).generate()
 
-    def package(self):
-      cmake = CMake(self)
-      cmake.install()
+  def build(self):
+    cmake = CMake(self)
+    cmake.configure()
+    cmake.build()
 
-    def deploy(self):
-      self.copy( "*" )
+  def package(self):
+    cmake = CMake(self)
+    cmake.install()
